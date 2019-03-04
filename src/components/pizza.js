4df1276023c8d0819f959a0bbaf4e7421fdcfa98
Benjamin Siegel <benjaminsiegel@Benjamins-MBP-2.home>
2019-03-04 13:22:11 -0500

    add name

--- src/components/pizza.js
+++ src/components/pizza.js
@@ -4,7 +4,7 @@ import './css/pizza.css'
 const Pizza = () => {
   return(
     <div className='Pizza'>
-      <h1 className='header-text'>Dollar Pizza</h1>
+      <h1 className='header-text'>Jeff's Dollar Pizza</h1>
     </div>
   )
 }
